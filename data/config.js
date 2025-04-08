acc5d60e3be3dac192aae53db587d8d7cb47aff3
Lindani Mabaso <lindaniM@synthesis.co.za>
2025-04-08 13:24:55 +0200

    update backend

--- data/config.js
+++ data/config.js
@@ -8,6 +8,7 @@ dotenv.config();
 // Set database connection credentials from environment variables
 const config = {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
